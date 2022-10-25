3a8d9d969c08b164569f9f778b65d3b3e380a433
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-25 13:13:21 +0100

    chore:added all the interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React, { useState } from "react";
 import data from "./data";
 function App() {
-  return <h2>lorem ipsum project </h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello world!");
+  };
+
+  return (
+    <section className="section-center">
+      <h2>Tired of boring Lorem Ipsum</h2>
+      <form className="lorem-form" onClick={handleSubmit}>
+        <label htmlFor="amount">paragraph</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta,
+          repudiandae.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta,
+          repudiandae.
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
